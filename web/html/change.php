96ab8d9f562c78f47a91bb8cc300962c0010ed47
Davidtu1112 <56072223+Davidtu1112@users.noreply.github.com>
2020-06-07 22:50:59 +0800

    Update change.php

--- web/html/change.php
+++ web/html/change.php
@@ -47,25 +47,7 @@
             </ul>
         </div>
     </nav>
-    <header>
-        <div id="demo" class="carousel slide" data-ride="carousel" style="width: 600px; margin: auto;">
-
-            <!-- Indicators -->
-            <ul class="carousel-indicators">
-                <li data-target="#demo" data-slide-to="0" class="active"></li>
-                <li data-target="#demo" data-slide-to="1"></li>
-                <li data-target="#demo" data-slide-to="2"></li>
-            </ul>
-
-            <!-- Left and right controls -->
-            <a class="carousel-control-prev" href="#demo" data-slide="prev">
-                <span class="carousel-control-prev-icon"></span>
-            </a>
-            <a class="carousel-control-next" href="#demo" data-slide="next">
-                <span class="carousel-control-next-icon"></span>
-            </a>
-        </div>
-    </header>
+    
     <section>
         <div class="container">
             <div class="row">
