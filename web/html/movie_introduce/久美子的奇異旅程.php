6fda52dbb5cf8f257586ea679a04c21957d5ba15
lincookie <44064149+lincookie@users.noreply.github.com>
2020-06-07 19:42:19 +0800

    Update 久美子的奇異旅程.php

--- web/html/movie_introduce/久美子的奇異旅程.php
+++ web/html/movie_introduce/久美子的奇異旅程.php
@@ -34,7 +34,7 @@
 
 <body style="font-family: Microsoft JhengHei; font-size: 17px;">
     <nav class="navbar navbar-expand-lg navbar-light bg-light">
-        <a class="navbar-brand" href="../html/Home.html" style="font-size: 25px;">電影院訂票系統</a>
+        <a class="navbar-brand" href="../html/Home.php" style="font-size: 25px;">電影院訂票系統</a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"> <span
         class="navbar-toggler-icon"></span> </button>
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
