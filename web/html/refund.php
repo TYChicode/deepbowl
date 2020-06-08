07be9bd836c12a9a08ddac90a14df9a3e4a0132c
Louis <johnny0816ray@gmail.com>
2020-06-08 15:21:51 +0800

    Update refund.php

--- web/html/refund.php
+++ web/html/refund.php
@@ -4,10 +4,15 @@
     $sql = "SELECT `id` FROM `customer_information` WHERE `email` LIKE '$email'";
     $result = $conn->query($sql);
     $row = $result->fetch_assoc();
-
     $id = $row["id"];
-    $sql = "Delete *  FROM `order_information` WHERE `customer_id`='$id'";
-    $result = $conn->query($sql);
+
+    if ($conn->query($sql) === TRUE) {
+        $sql = "Delete *  FROM `order_information` WHERE `customer_id`='$id'";
+        echo("<script>alert('刪除完成')</script>");
+        echo("<script>window.location = 'SingUp.php';</script>");
+    } else {
+        echo "Error: " . $sql . "<br>" . $conn->error;
+    }
 
     mysqli_close($conn);
 ?>
